1e4672529602a1110afc3c804a758225317a6b28
Rahulbanjare95 <rahul.banjare95@gmail.com>
2020-06-18 16:20:06 +0530

    Added: editPerson() to edit details of a Person

--- src/com/Bridgelabz/Person.java
+++ src/com/Bridgelabz/Person.java
@@ -1,78 +1,79 @@
 package com.Bridgelabz;
- public  class Person {
-    private String firstName;
-    private String lastName;
-    public String city;
-    public String state;
-    public int zipCode;
-    public String phoneNumber;
 
-    public Person(String firstName, String lastName, String city, String state, int zipCode, String phoneNumber) {
-        this.firstName = firstName;
-        this.lastName = lastName;
-        this.city = city;
-        this.state = state;
-        this.zipCode = zipCode;
-        this.phoneNumber = phoneNumber;
-    }
+public  class Person {
+        private String firstName;
+        private String lastName;
+        public String city;
+        public String state;
+        public String zipCode;
+        public String phoneNumber;
 
-    public String getLastName() {
-        return lastName;
-    }
+        public Person(String firstName, String lastName, String city, String state, String zipCode, String phoneNumber) {
+            this.firstName = firstName;
+            this.lastName = lastName;
+            this.city = city;
+            this.state = state;
+            this.zipCode = zipCode;
+            this.phoneNumber = phoneNumber;
+        }
 
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
+        public String getLastName(String lname) {
+            return lastName;
+        }
 
-    public String getCity() {
-        return city;
-    }
+        public void setLastName(String lastName) {
+            this.lastName = lastName;
+        }
 
-    public void setCity(String city) {
-        this.city = city;
-    }
+        public String getCity() {
+            return city;
+        }
 
-    public String getState() {
-        return state;
-    }
+        public void setCity(String city) {
+            this.city = city;
+        }
 
-    public void setState(String state) {
-        this.state = state;
-    }
+        public String getState() {
+            return state;
+        }
 
-    public int getZipCode() {
-        return zipCode;
-    }
+        public void setState(String state) {
+            this.state = state;
+        }
 
-    public void setZipCode(int zipCode) {
-        this.zipCode = zipCode;
-    }
+        public String getZipCode() {
+            return zipCode;
+        }
 
-    public String getPhoneNumber() {
-        return phoneNumber;
-    }
+        public void setZipCode(String zipCode) {
+            this.zipCode = zipCode;
+        }
 
-    public void setPhoneNumber(String phoneNumber) {
-        this.phoneNumber = phoneNumber;
-    }
+        public String getPhoneNumber() {
+            return phoneNumber;
+        }
 
-    public String getFirstName() {
-        return firstName;
-    }
+        public void setPhoneNumber(String phoneNumber) {
+            this.phoneNumber = phoneNumber;
+        }
 
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
+        public String getFirstName(String firstname) {
+            return firstName;
+        }
 
-    @Override
-    public String toString() {
-        return "Person{" +
-                "firstName='" + firstName + '\'' +
-                ", lastName='" + lastName + '\'' +
-                ", city='" + city + '\'' +
-                ", state='" + state + '\'' +
-                ", zipCode=" + zipCode +
-                ", phoneNumber=" + phoneNumber +
-                '}';
+        public void setFirstName(String firstName) {
+            this.firstName = firstName;
+        }
+
+        @Override
+        public String toString() {
+            return "Person{" +
+                    "firstName='" + firstName + '\'' +
+                    ", lastName='" + lastName + '\'' +
+                    ", city='" + city + '\'' +
+                    ", state='" + state + '\'' +
+                    ", zipCode=" + zipCode +
+                    ", phoneNumber=" + phoneNumber +
+                    '}';
+        }
     }
-}
