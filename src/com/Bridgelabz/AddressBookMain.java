286771d7fca2c033af10d2fe2e34de9346d466e7
Rahulbanjare95 <rahul.banjare95@gmail.com>
2020-06-19 02:49:55 +0530

    Modified: added counter to create new PersonInfo

--- src/com/Bridgelabz/AddressBookMain.java
+++ src/com/Bridgelabz/AddressBookMain.java
@@ -93,7 +93,6 @@ public class AddressBookMain{
                 System.out.println("No name found");
         }
     }
-
     public static void main(String[] args)
     {
         boolean counter=false;
