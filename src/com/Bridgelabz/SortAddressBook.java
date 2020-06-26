ff221ef83b8e9f91be575fd77c137439270a44ca
Rahulbanjare95 <rahul.banjare95@gmail.com>
2020-06-26 22:23:30 +0530

    Refactored: Sorting techniques using JAVA 8 features

--- src/com/Bridgelabz/SortAddressBook.java
+++ src/com/Bridgelabz/SortAddressBook.java
@@ -1,38 +1,32 @@
 package com.Bridgelabz;
-
 import java.util.ArrayList;
-import java.util.Collection;
-import java.util.Collections;
+import java.util.Comparator;
 
 public class SortAddressBook {
-    public static  void sortByFirstName(ArrayList<Person>p){
-        Collections.sort(p,Person.nameSort);
-        for (Person psort:p){
-            System.out.println(psort);
+    public static  void sortByFirstName(ArrayList<Person>personInfo){
+        personInfo.sort(Comparator.comparing(Person::getFirstName));
+        printAllSorted(personInfo,element->true);
+    }
+    public static void sortByCity(ArrayList<Person>personInfo){
+        personInfo.sort(Comparator.comparing(Person::getCity));
+        printAllSorted(personInfo,element->true);
+    }
+    public static void sortByZip(ArrayList<Person>personInfo) {
+        personInfo.sort(Comparator.comparing(Person::getZipCode));
+        printAllSorted(personInfo,element->true);
+    }
+    public static void sortByState(ArrayList<Person>personInfo){
+        personInfo.sort(Comparator.comparing(Person::getState));
+        printAllSorted(personInfo,element->true);
+    }
+    public static void printAllSorted(ArrayList<Person> people, Checker c){
+        for (Person p:people) {
+            if (c.test(p)){
+                System.out.println(p);
+            }
         }
     }
-    public static void sortByCity(ArrayList<Person>p){
-        Collections.sort(p,Person.citySort);
-        for (Person psort:p){
-            System.out.println(psort);
-        }
-
-    }
-    public static void sortByZip(ArrayList<Person>p){
-        Collections.sort(p,Person.zipSort);
-        for (Person psort:p) {
-            System.out.println(psort);
-
-        }
-    };
-    public static void sortByState(ArrayList<Person>p){
-        Collections.sort(p,Person.stateSort);
-        for (Person psort:p) {
-            System.out.println(psort);
-
-        }
-    };
-
-
-
+}
+interface Checker {
+    boolean test(Person p);
 }
