b057df1c557d9d82b7a8682f806d4491d32fa916
Rahulbanjare95 <rahul.banjare95@gmail.com>
2020-07-31 05:09:00 +0530

    Added: Test case and code to write Json object to a file while adding person information

---
+++ src/main/java/com/Bridgelabz/main/exceptions/AddressBookException.java
@@ -0,0 +1,15 @@
+package com.Bridgelabz.main.exceptions;
+
+public class AddressBookException extends Exception {
+
+    public enum ExceptionType {ALREADY_PRESENT}
+
+    private final ExceptionType type;
+
+    public AddressBookException(String message, ExceptionType type) {
+        super(message);
+        this.type = type;
+    }
+
+
+}
--- src/test/java/AddressBookTest.java
+++ src/test/java/AddressBookTest.java
@@ -1,16 +1,23 @@
 import com.Bridgelabz.main.AddressBook;
 import com.Bridgelabz.models.Person;
-import org.junit.Assert;
 import org.junit.Test;
+
+import java.io.IOException;
+import java.util.ArrayList;
 
 public class AddressBookTest {
     @Test
-    public void givenAJsonFile_WhenAddedInDirectory_ShouldReturnTrue() {
+    public void givenAPersonDetail_WhenAdded_ShouldWriteIntoJsonFile() {
         AddressBook addressBookMain = new AddressBook();
         Person person = new Person("Rahul", "Banjare", "Raipur", "Chhattisgarh", "45321", "8982423482");
-        int i = addressBookMain.addPersonInfo(person);
-        Assert.assertEquals(1,i);
+        try {
+            ArrayList<Person> people = addressBookMain.addPersonInfo(person);
+            addressBookMain.writeToJson("addressBook.json");
 
-
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
+
+    
 }
