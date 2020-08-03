7a522e7b8b4f782270004bdb68274709fc51e3ac
Rahulbanjare95 <rahul.banjare95@gmail.com>
2020-08-03 08:07:02 +0530

    Added: Json files to read from json server

--- src/test/java/AddressBookTest.java
+++ src/test/java/AddressBookTest.java
@@ -1,20 +1,48 @@
+package com.bridgelabz.addressbook.test;
 
-import com.Bridgelabz.models.Person;
-import com.Bridgelabz.services.AddressBook;
-import org.junit.Assert;
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
+import org.hamcrest.Matchers;
+import org.junit.Before;
 import org.junit.Test;
 
-//public class AddressBookTest {
-//    @Test
-//    public void givenAPersonDetail_whenAddedTwoTimes_ShouldThrowException() {
-//        AddressBook addressBookMain = new AddressBook();
-//        Person person = new Person("Rahul", "Banjare", "Raipur", "Chhattisgarh", "45321", "8982423482");
-//        Person personone = new Person("Rahul", "Banjare", "Raipur", "Chhattisgarh", "45321", "8982423482");
-//        try {
-//            addressBookMain.addPersonInfo(person);
-//            addressBookMain.addPersonInfo(personone);
-//        } catch (AddressBookException e) {
-//            Assert.assertEquals(AddressBookException.ExceptionType.ALREADY_PRESENT,e.type);
-//        }
-//    }
-//}
+public class AddressBookTest {
+
+
+    @Before
+    public void setUp() {
+        RestAssured.baseURI = "http://localhost:4000";
+    }
+
+    public Response getPersonList() {
+        return RestAssured.get("/addressBook/list");
+    }
+
+    @Test
+    public void givenPerson_WhenPresent_ShouldReturnDetails() {
+        Response response = getPersonList();
+        System.out.println(response.asString());
+        response.then().body("id", Matchers.hasItems(1));
+        response.then().body("firstName", Matchers.hasItems("Rahul"));
+    }
+
+    @Test
+    public void givenPerson_OnPost_ShouldReturnAddedPerson() {
+        Response response = RestAssured.given()
+                .contentType(ContentType.JSON)
+                .accept(ContentType.JSON)
+                .body("{\"firstName\": \"Vaibhav\", \"lastName\": \"Singh\"," +
+                        "\"city\": \"korba\", \"state\": \"cg\", \"zipCode\": \"456737\"," +
+                        "\"phone\": \"8967654398\"}")
+                .when()
+                .post("/addressBook/create");
+        response.then().body("firstName", Matchers.is("Vaibhav"));
+    }
+
+    @Test
+    public void givenPersonId_OnDelete_ShouldReturnSuccessStatus() {
+        Response response = RestAssured.delete("/addressBook/delete/3");
+        response.then().body("id", Matchers.not(2));
+    }
+}
